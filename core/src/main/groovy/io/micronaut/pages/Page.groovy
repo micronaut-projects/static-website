dd8fe31075735d3ad5bee07129f9eb205c37f456
sdelamo <sergio.delamo@softamo.com>
2018-05-24 07:48:47 +0200

    favicon

--- core/src/main/groovy/io/micronaut/pages/Page.groovy
+++ core/src/main/groovy/io/micronaut/pages/Page.groovy
@@ -229,6 +229,7 @@ abstract class Page implements HtmlPage {
                 meta name: 'description', content: getMetaDescription()
             }
             meta charset: 'utf-8'
+            link rel: 'icon', href: "${micronautUrl()}/${getImageAssetPreffix()}favicon.ico"
             meta name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
             meta name: 'twitter:card', content: 'summary_large_image'
             meta name: "twitter:site", content: '@micronautfw'
