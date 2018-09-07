a2b8d6dd12a65e4409d3342112025b81ab74a7da
Sergio del Amo <sergio.delamo@softamo.com>
2018-09-07 20:21:47 +0200

    Retargeting pixel

--- core/src/main/groovy/io/micronaut/pages/Page.groovy
+++ core/src/main/groovy/io/micronaut/pages/Page.groovy
@@ -311,6 +311,26 @@ abstract class Page implements HtmlPage {
   gtag('js', new Date());
   gtag('config', '${GOOGLE_ANALYTICS_TRACKING_CODE}');
 </script>
+<script type="text/javascript">
+    adroll_adv_id = "J4NA4XCFDNFHDGNFUAVMQD";
+    adroll_pix_id = "J4XYMNCOQNBCXJJ4GP744N";
+
+    (function () {
+        var _onload = function(){
+            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+            var scr = document.createElement("script");
+            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+            scr.setAttribute('async', 'true');
+            scr.type = "text/javascript";
+            scr.src = host + "/j/roundtrip.js";
+            ((document.getElementsByTagName('head') || [null])[0] ||
+                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+        };
+        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+        else {window.attachEvent('onload', _onload)}
+    }());
+</script>
 """
     }
 
