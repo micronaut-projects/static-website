38bd89dbebda32858c0edcc3443c64e6add27c43
sdelamo <sergio.delamo@softamo.com>
2019-01-15 09:27:15 +0100

    Add google cloud guides category

--- guides/src/main/groovy/io/micronaut/guides/pages/GuidesPage.groovy
+++ guides/src/main/groovy/io/micronaut/guides/pages/GuidesPage.groovy
@@ -339,6 +339,7 @@ class GuidesPage extends Page implements ReadFileUtils {
                         }
                         if ( !(tag || category) ) {
                             mkp.yieldUnescaped guideGroupByCategory(categories().apprentice, guides, true, "margin-top: ${MARGIN_TOP};")
+                            mkp.yieldUnescaped guideGroupByCategory(categories().googlecloud, guides, true, "margin-top: ${MARGIN_TOP};")
                             mkp.yieldUnescaped guideGroupByCategory(categories().cloudservices, guides, true, "margin-top: ${MARGIN_TOP};")
                             mkp.yieldUnescaped guideGroupByCategory(categories().dataaccess, guides, true, "margin-top: ${MARGIN_TOP};")
                         }
@@ -351,6 +352,7 @@ class GuidesPage extends Page implements ReadFileUtils {
 
     static Map<String, Category> categories() {
         [
+                googlecloud: new Category(name: "Micronaut + Google Cloud", image: 'googlecloud.svg'),
             android: new Category(name: "Micronaut Android", image: 'micronaut_android.svg'),
             devops: new Category(name: "Micronaut DevOps", image: 'micronaut_devops.svg'),
             apprentice: new Category(name: "Micronaut Apprentice", image: 'micronautaprrentice.svg'),
