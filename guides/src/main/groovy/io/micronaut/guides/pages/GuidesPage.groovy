657d3dabf43c5b8b7b9cd44274dd0d470b60cb7e
Sergio del Amo <sergio.delamo@softamo.com>
2019-06-11 14:14:24 +0200

    Add Micronaut + Microsoft Azure category

--- guides/src/main/groovy/io/micronaut/guides/pages/GuidesPage.groovy
+++ guides/src/main/groovy/io/micronaut/guides/pages/GuidesPage.groovy
@@ -339,6 +339,7 @@ class GuidesPage extends Page implements ReadFileUtils {
                         }
                         if ( !(tag || category) ) {
                             mkp.yieldUnescaped guideGroupByCategory(categories().apprentice, guides, true, "margin-top: ${MARGIN_TOP};")
+                            mkp.yieldUnescaped guideGroupByCategory(categories().azure, guides, true, "margin-top: ${MARGIN_TOP};")
                             mkp.yieldUnescaped guideGroupByCategory(categories().googlecloud, guides, true, "margin-top: ${MARGIN_TOP};")
                             mkp.yieldUnescaped guideGroupByCategory(categories().cloudservices, guides, true, "margin-top: ${MARGIN_TOP};")
                             mkp.yieldUnescaped guideGroupByCategory(categories().dataaccess, guides, true, "margin-top: ${MARGIN_TOP};")
@@ -352,6 +353,7 @@ class GuidesPage extends Page implements ReadFileUtils {
 
     static Map<String, Category> categories() {
         [
+                azure: new Category(name: "Micronaut + Microsoft Azure", image: 'azure.svg'),
                 googlecloud: new Category(name: "Micronaut + Google Cloud", image: 'googlecloud.svg'),
             android: new Category(name: "Micronaut Android", image: 'micronaut_android.svg'),
             devops: new Category(name: "Micronaut DevOps", image: 'micronaut_devops.svg'),
