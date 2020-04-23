9d052b5a675772d29a88bcdc14c8f380c10d1dea
sdelamo <delamos@objectcomputing.com>
2020-04-23 14:56:03 +0000

    Updating main Micronaut site for Travis build: https://travis-ci.org/micronaut-projects/static-website/builds/678633267

---
+++ start/precache-manifest.7cb82e929b21ebacf72ab92c6245af15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d039ccb31ae5b3736740454587f0549c",
+    "url": "/start/index.html"
+  },
+  {
+    "revision": "138ee37b54a14f3f7cf0",
+    "url": "/start/static/css/main.9c17d6e7.chunk.css"
+  },
+  {
+    "revision": "4b6069d635de135533ad",
+    "url": "/start/static/js/2.c3ce32eb.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/start/static/js/2.c3ce32eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138ee37b54a14f3f7cf0",
+    "url": "/start/static/js/main.268a00b1.chunk.js"
+  },
+  {
+    "revision": "051236882a997fdc72c0",
+    "url": "/start/static/js/runtime-main.65fefded.js"
+  },
+  {
+    "revision": "e89033cfaaef0afa2e6fa0e47d6f4624",
+    "url": "/start/static/media/micronaut.e89033cf.png"
+  }
+]);
