4cf3a4d9a59025f78b8b7dd7eb0b9a48b17b0ea1
Miguel Ángel Quintanilla <mangel@profile.es>
2020-04-23 13:25:37 +0200

    Add starter

---
+++ main/src/main/js/start/README.md
@@ -0,0 +1,9 @@
+# Micronaut Inizializr APP
+
+Frontend module of Micronaut Starter. Developed with React.js by Profile Software Services.
+
+## Run
+
+```npm install```
+
+```npm start```
---
+++ main/src/main/js/start/src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://cors-anywhere.herokuapp.com/https://micronaut-starter-staging-ucxwqnh6ka-uc.a.run.app"
+export const JAVA_VERSIONS = [8, 9, 10, 11, 12, 13, 14]
