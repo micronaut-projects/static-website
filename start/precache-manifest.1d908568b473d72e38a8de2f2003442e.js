7c57a9fb5661a3271ebdeaec8ec99983b18e0518
sdelamo <delamos@objectcomputing.com>
2020-04-24 20:10:41 +0000

    Updating main Micronaut site for Travis build: https://travis-ci.org/micronaut-projects/static-website/builds/679186762

---
+++ start/precache-manifest.1d908568b473d72e38a8de2f2003442e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d06e731408b1c918a9cce321c907a1",
+    "url": "/start/index.html"
+  },
+  {
+    "revision": "629638b91eb49d1a43db",
+    "url": "/start/static/css/main.88a799a1.chunk.css"
+  },
+  {
+    "revision": "76cc868e045921d7dd74",
+    "url": "/start/static/js/2.f9414ed9.chunk.js"
+  },
+  {
+    "revision": "d98ecab7398de6a4678f9230a85242ea",
+    "url": "/start/static/js/2.f9414ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629638b91eb49d1a43db",
+    "url": "/start/static/js/main.d85174ba.chunk.js"
+  },
+  {
+    "revision": "051236882a997fdc72c0",
+    "url": "/start/static/js/runtime-main.65fefded.js"
+  },
+  {
+    "revision": "e89033cfaaef0afa2e6fa0e47d6f4624",
+    "url": "/start/static/media/micronaut.e89033cf.png"
+  }
+]);
--- start/service-worker.js
+++ start/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/start/precache-manifest.fa29d0a8143adfe3400198f2e4670784.js"
+  "/start/precache-manifest.1d908568b473d72e38a8de2f2003442e.js"
 );
 
 self.addEventListener('message', (event) => {
