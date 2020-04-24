cd3d0b25941319ff3a06f57369641e78f98831b4
jameskleeh <james.kleeh@gmail.com>
2020-04-24 16:05:58 -0400

    Fix server uri

--- main/src/main/js/start/src/constants/index.js
+++ main/src/main/js/start/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = "http://micronaut-starter-staging-ucxwqnh6ka-uc.a.run.app";
 export const JAVA_VERSIONS = [8, 9, 10, 11, 12, 13, 14];
 export const DEFAULT_JAVA_VERSION = 11;
 export const DEFAULT_LANG = "java";
