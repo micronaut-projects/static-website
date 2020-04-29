44e5ab557517c8e07012d5cf60184acc958ed30f
James Kleeh <james.kleeh@gmail.com>
2020-04-29 19:09:57 -0400

    Update TooltipButton.js

--- main/src/main/js/launch/src/components/TooltipButton.js
+++ main/src/main/js/launch/src/components/TooltipButton.js
@@ -5,7 +5,7 @@ import { Button } from "react-materialize";
 
 const TooltipButton = ({ tooltip, children, ...props }) => {
     return (
-        <Tooltip enterDelay={1000} enterTouchDelay={300} title={tooltip}>
+        <Tooltip enterDelay={500} enterTouchDelay={300} title={tooltip}>
             <span>
                 <Button {...props}>{children}</Button>
             </span>
