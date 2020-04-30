23ef476eef90184ece4683a4818bb2b40ba0fbce
Eldon Ahrold <eldon.ahrold@gmail.com>
2020-04-30 10:36:51 -0500

    Move modal styling into `styles/modal-overrides.css` file

--- main/src/main/js/launch/src/App.jsx
+++ main/src/main/js/launch/src/App.jsx
@@ -27,6 +27,7 @@ import { makeNodeTree } from "./utility";
 
 import "./style.css";
 import "./styles/button-row.css";
+import "./styles/modal-overrides.css";
 
 class App extends Component {
   constructor(props) {
