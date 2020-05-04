024149d01ad71227a6b1c1773bd585f52c421250
Iván López <lopezi@objectcomputing.com>
2020-05-04 09:41:45 +0200

    Fix documentation link.
    Closes #66

--- main/src/main/groovy/io/micronaut/main/pages/HomePage.groovy
+++ main/src/main/groovy/io/micronaut/main/pages/HomePage.groovy
@@ -35,7 +35,7 @@ class HomePage extends PageWithSyntaxHighlight {
             div(id: "calltoactionbuttons") {
                 a(href: 'https://micronaut.io/launch/', 'Generate a Project')
                 a(href: 'https://micronaut.io/download.html', 'Download the Framework')
-                a(href: 'https://micronaut.io/download.html', 'Read the Documentation')
+                a(href: 'https://micronaut.io/documentation.html', 'Read the Documentation')
             }
             div(id: "calltoaction") {
                 h2 'A modern, JVM-based, full-stack framework for building modular, easily testable microservice and serverless applications.'
