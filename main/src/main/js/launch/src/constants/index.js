2994795a6b2771dc7e076af5b8e31a4e9e5da8aa
Graeme Rocher <graeme.rocher@gmail.com>
2020-06-16 13:19:44 +0200

    Update to 2.0.0 RC1

--- main/src/main/js/launch/src/constants/index.js
+++ main/src/main/js/launch/src/constants/index.js
@@ -8,8 +8,8 @@ export const JAVA_VERSIONS = [8, 9, 10, 11, 12, 13, 14];
 
 export const MICRONAUT_VERSIONS = [
     {
-        label: "2.0.0.M3",
-        value: "2.0.0.M3",
+        label: "2.0.0.RC1",
+        value: "2.0.0.RC1",
         api: API_URL,
     },
     {
