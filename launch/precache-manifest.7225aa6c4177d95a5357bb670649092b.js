52d026147c6e28d5f9841e2d6059f13c15342c23
sdelamo <sdelamo@users.noreply.github.com>
2020-07-27 14:36:07 +0000

    Updating micronaut-projects/static-website gh-pages branch for Github Actions run:184389696

---
+++ launch/precache-manifest.7225aa6c4177d95a5357bb670649092b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e139ad43c7f9df75b436d1fad598c3e",
+    "url": "/launch/index.html"
+  },
+  {
+    "revision": "ef14833a652f8a9635e0",
+    "url": "/launch/static/css/main.136df343.chunk.css"
+  },
+  {
+    "revision": "8dfcb1c764c4844ce6a4",
+    "url": "/launch/static/js/2.6a46ffde.chunk.js"
+  },
+  {
+    "revision": "b50fd088e9117250a094f2cf6301ce88",
+    "url": "/launch/static/js/2.6a46ffde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef14833a652f8a9635e0",
+    "url": "/launch/static/js/main.1bb56907.chunk.js"
+  },
+  {
+    "revision": "4f6bf22283729bd8fdf3",
+    "url": "/launch/static/js/runtime-main.ac23779b.js"
+  },
+  {
+    "revision": "6d5a65223989e6bc7c1fae49d5a0210f",
+    "url": "/launch/static/media/micronaut-white-icon.6d5a6522.png"
+  }
+]);
--- launch/service-worker.js
+++ launch/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/launch/precache-manifest.d281b5be14cb18a8b0def9abb3c26b38.js"
+  "/launch/precache-manifest.7225aa6c4177d95a5357bb670649092b.js"
 );
 
 self.addEventListener('message', (event) => {
