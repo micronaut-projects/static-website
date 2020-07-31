a9e2f82ec16e4e2757f4908ed2937cdcdb42151e
Eldon Ahrold <ahrolde@objectcomputing.com>
2020-07-31 05:25:26 -0500

    Micronaut logos gallery Feat. (#113)
    * Create Micronaut logo gallery page #36
    
    closes https://github.com/micronaut-projects/static-website-private/issues/36
    
    * Create Micronaut logo gallery page #36
    
    closes https://github.com/micronaut-projects/static-website-private/issues/36
    
    * Create Micronaut logo gallery page - clean-up CopyAssetTask
    
    * Create Micronaut logo gallery page - Update usage permission text.
    
    * add type
    
    * remove unnecessary semicolon
    
    * remove optional return statement
    
    * use type instead of def
    
    * annotate with compile static
    
    * clean depends on cleanLogos
    
    * rename param
    
    * rename method
    
    * change description of logos task
    
    * remove implicit return statement
    
    * use string interpolation
    
    * use flatten to simplify
    
    * rename to LogosTask
    
    * don't create extra span
    
    * del welcome to the micronaut logo gallery header
    
    * rename micronaut_logos.yml to just logos.yml
    
    * Cleanup string interpolation in MicronautLogo
    
    * Cleanup string interpolation in LogosTask
    
    * Update LogosTask task description
    
    * Rename `renderDocsPage` to `renderQuestionsPage` in QuestionsTask
    
    * Update Micronaut Data Logos
    
    * in config svg, png and thumb
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

---
+++ buildSrc/src/main/groovy/io/micronaut/gallery/MicronautLogo.groovy
@@ -0,0 +1,12 @@
+package io.micronaut.gallery
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class MicronautLogo implements MicronautLogoHtml {
+    String thumb
+    String png
+    String svg
+    String label
+    String background
+}
